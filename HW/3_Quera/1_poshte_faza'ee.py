b626ced5ab41d940d58e2d9ea8b8a56c74b9aec7
Ali Nazari <ali.nazari@aut.ac.ir>
2018-10-23 14:40:13 +0330

    change the stack class to a simple stack and use 2 stack for finding the minimum of unpoped elements

--- HW/3_Quera/1_poshte_faza'ee.py
+++ HW/3_Quera/1_poshte_faza'ee.py
@@ -1,24 +1,12 @@
 class Stack:
     def __init__(self):
         self.list = []
-        self.minimum = 0
 
     def push(self, element):
-        if self.is_empty:
-            self.minimum = element
-            self.list.append(element)
-        else:
-            if self.minimum <= element:
-                self.list.append(element)
-            else:
-                self.minimum = element
-                self.list.append(element)
+        self.list.append(element)
 
     def pop(self):
         return self.list.pop()
-
-    def spell(self):
-        return self.minimum
 
     def is_empty(self):
         if self.list.__len__() == 0:
@@ -27,19 +15,34 @@ class Stack:
             return False
 
 
-stack = Stack()
+main_stack = Stack()
+min_stack = Stack()
+
 q = int(input())
 commands = [''] * q
+
 for i in range(q):
     commands[i] = input()
 
 for j in range(q):
     if commands[j].__contains__("push"):
         push_array = commands[j].split()
-        stack.push(push_array[1])
-        print(push_array[1])
+        main_stack.push(push_array[1])
+
+        if min_stack.is_empty():
+            min_stack.push(push_array[1])
+        else:
+            element_min = min_stack.pop()
+            min_stack.push(element_min)
+            if push_array[1] < element_min:
+                min_stack.push(push_array[1])
+
     if commands[j].__contains__("pop"):
-        # print("pop " + stack.pop())
-        stack.pop()
+        element_stack = main_stack.pop()
+        element_min = min_stack.pop()
+        if element_min != element_stack:
+            min_stack.push(element_min)
     if commands[j].__contains__("spell"):
-        print("spell " + stack.spell())
+        element_min = min_stack.pop()
+        print(element_min)
+        min_stack.push(element_min)
