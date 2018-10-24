e1631da44eaa02ae5358bbe3d3bd7d426b69f341
Ali Nazari <ali.nazari@aut.ac.ir>
2018-10-24 11:38:35 +0330

    finish kasif algorithm

--- HW/3_Quera/1_poshte_faza'ee_plus.py
+++ HW/3_Quera/1_poshte_faza'ee_plus.py
@@ -28,7 +28,7 @@ min = 0
 for j in range(q):
     if command[j].__contains__("push"):
         push_array = command[j].split()
-        element_stack = push_array[1]
+        element_stack = int(push_array[1])
 
         if main_stack.is_empty():
             min = element_stack
@@ -38,19 +38,11 @@ for j in range(q):
         main_stack.push(element_stack)
 
     if command[j].__contains__("pop"):
-        element_stack = main_stack.pop()
-        if element_stack == min:
-            temp = Stack()
-            temp_element = main_stack.pop()
-            min = temp_element
-            main_stack.push(temp_element)
-            while not main_stack.is_empty():
-                temp_element = main_stack.pop()
-                if temp_element < min:
-                    min = temp_element
-                temp.push(temp_element)
-            while not temp.is_empty():
-                main_stack.push(temp.pop())
+        if main_stack.pop() == min:
+            min = main_stack.list[0]
+            for i in main_stack.list:
+                if min > i:
+                    min = i
 
     if command[j].__contains__("spell"):
         print(min)
