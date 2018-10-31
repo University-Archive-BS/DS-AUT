61e96d5a49687e243ee7f3a1ac86584f5c2fdea4
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-01 01:02:49 +0330

    improve HW3 Q4 jacub

--- HW/3_Quera/Q4/4_jacub_barqi.py
+++ HW/3_Quera/Q4/4_jacub_barqi.py
@@ -7,18 +7,21 @@ tail = stuffs.__len__() - 1
 head = 0
 temp = 0
 while temp != n:
+    # print(int(stuffs[head]) + int(stuffs[tail]))
     if head == tail:
         temp += 1
         boxes += 1
-    elif int(stuffs[head]) + int(stuffs[tail]) <= x:
+        # print("if " + str(boxes))
+    elif int(stuffs[head]) + int(stuffs[tail]) <= int(x):
         boxes += 1
         head += 1
         tail -= 1
         temp += 2
+        # print("elif " + str(boxes))
     else:
         temp += 1
         boxes += 1
-        head += 1
-
+        tail -= 1
+        # print("else " + str(boxes))
 
 print(boxes)
