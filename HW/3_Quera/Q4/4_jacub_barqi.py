793ecc9f94d0158752c716bf1c64fcc074732e3b
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-01 01:02:09 +0330

    improve HW3 Q4 jacub

--- HW/3_Quera/Q4/4_jacub_barqi.py
+++ HW/3_Quera/Q4/4_jacub_barqi.py
@@ -2,17 +2,23 @@ n, x = map(int, input().split())
 stuffs = input().split()
 stuffs.sort()
 
-temp = 0
+boxes = 0
 tail = stuffs.__len__() - 1
 head = 0
-while head != tail:
-    if int(stuffs[head]) + int(stuffs[tail]) <= x:
+temp = 0
+while temp != n:
+    if head == tail:
         temp += 1
+    elif int(stuffs[head]) + int(stuffs[tail]) <= x:
+        boxes += 1
         head += 1
         tail -= 1
+        temp += 2
     else:
         temp += 1
+        boxes += 1
         head += 1
 
 
-print(temp)
+print(boxes)
+
