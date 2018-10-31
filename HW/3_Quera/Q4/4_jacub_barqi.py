50ca551b376021dff17b4d32ba1759718012564c
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-01 01:03:13 +0330

    improve HW3 Q4 jacub and refactor

--- HW/3_Quera/Q4/4_jacub_barqi.py
+++ HW/3_Quera/Q4/4_jacub_barqi.py
@@ -1,27 +1,23 @@
 n, x = map(int, input().split())
 stuffs = input().split()
-stuffs.sort()
+stuffs.sort(reverse=True)
 
 boxes = 0
 tail = stuffs.__len__() - 1
 head = 0
 temp = 0
 while temp != n:
-    # print(int(stuffs[head]) + int(stuffs[tail]))
     if head == tail:
         temp += 1
         boxes += 1
-        # print("if " + str(boxes))
     elif int(stuffs[head]) + int(stuffs[tail]) <= int(x):
         boxes += 1
         head += 1
         tail -= 1
         temp += 2
-        # print("elif " + str(boxes))
     else:
         temp += 1
         boxes += 1
-        tail -= 1
-        # print("else " + str(boxes))
+        head += 1
 
 print(boxes)
