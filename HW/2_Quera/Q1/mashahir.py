aa292cd8ca0e0750053f5f7c67d3ff0c1dec5f84
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-22 17:28:40 +0330

    a youtube heap sort implementation added

--- HW/2_Quera/Q1/mashahir.py
+++ HW/2_Quera/Q1/mashahir.py
@@ -1,40 +1,45 @@
-class Node:
-    def __init__(self, value, father):
-        self.value = value
-        self.father = father
-        self.leftChild = None
-        self.rightChild = None
+def heap_sort(your_list):
+    your_list = make_max_heap(your_list)
+    elements = your_list.__len__()
+    count = elements - 1
+    while count >= 0:
+        temp_oona = your_list[0]
+        your_list[0] = your_list[count]
+        your_list[count] = temp_oona
+        count -= 1
+        heapify(your_list, 0)
+    return your_list
 
-    def has_child(self):
-        if self.leftChild or self.rightChild:
-            return True
-        else:
-            return False
 
-class BinarySearchTree:
-    def __init__(self):
-        self.root = None
-        self.size = 0
+def make_max_heap(your_list):
+    elements = your_list.__len__()
+    tt = int(elements / 2)
+    while tt > 0:
+        heapify(your_list, tt)
+        tt -= 1
+    return your_list
 
-    def insert(self, value):
-        if self.root is None:
-            self.root = Node(value, None)
-        else:
-            self.insert_node(self.root, value)
 
-        self.size += 1
+def heapify(your_list, element):
+    elements = your_list.__len__() - 1
+    left = 2 * element
+    right = 2 * element + 1
 
-    def insert_node(self, currentNode, value):
-        if value <= currentNode.value:
-            if currentNode.leftChild:
-                self.insert_node(currentNode.leftChild, value)
-            else:
-                currentNode.leftChild = Node(value, currentNode)
-        else:
-            if currentNode.rightChild:
-                self.insert_node(currentNode.rightChild, value)
-            else:
-                currentNode.rightChild = Node(value, currentNode)
+    max_ma = 0
+    if left < elements and your_list[left] > your_list[element]:
+        max_ma = left
+    else:
+        max_ma = element
+
+    if right <= elements and your_list[right] > your_list[max_ma]:
+        max_ma = right
+
+    if max_ma != element:
+        temp_ma = your_list[max_ma]
+        your_list[max_ma] = your_list[element]
+        your_list[element] = temp_ma
+        heapify(your_list, max_ma)
+    return your_list
 
 
 n = int(input())
@@ -43,25 +48,14 @@ command = []
 for i in range(n):
     command.append(input())
 
-my_bst = BinarySearchTree()
+my_list = []
 
 for j in range(n):
     if command[j].__len__() == 1:
         if j < 3:
             print("No reviews yet")
         else:
-            max_node = my_bst.root
-            while max_node.has_child:
-                max_node = max_node.rightChild
-
-            result = max_node
-            k = 1
-            limit = int(my_bst.size / 3)
-            while k < limit:
-                if result
-                k += 1
-            print(result.value)
-
-
+            my_list = heap_sort(my_list)
+            print(my_list)
     else:
-        my_bst.insert(int(command[j].split(" ")[1]))
+        my_list.append(int(command[j].split(" ")[1]))
