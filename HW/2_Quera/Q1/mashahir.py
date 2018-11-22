d50125e7aa0138aa3c519b54ddb76d7dfdc6dc4c
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-22 21:26:48 +0330

    implement my first think for Q2

--- HW/2_Quera/Q1/mashahir.py
+++ HW/2_Quera/Q1/mashahir.py
@@ -1,46 +1,4 @@
-def heap_sort(your_list):
-    your_list = make_max_heap(your_list)
-    elements = your_list.__len__()
-    count = elements - 1
-    while count >= 0:
-        temp_oona = your_list[0]
-        your_list[0] = your_list[count]
-        your_list[count] = temp_oona
-        count -= 1
-        heapify(your_list, 0)
-    return your_list
-
-
-def make_max_heap(your_list):
-    elements = your_list.__len__()
-    tt = int(elements / 2)
-    while tt > 0:
-        heapify(your_list, tt)
-        tt -= 1
-    return your_list
-
-
-def heapify(your_list, element):
-    elements = your_list.__len__() - 1
-    left = 2 * element
-    right = 2 * element + 1
-
-    max_ma = 0
-    if left < elements and your_list[left] > your_list[element]:
-        max_ma = left
-    else:
-        max_ma = element
-
-    if right <= elements and your_list[right] > your_list[max_ma]:
-        max_ma = right
-
-    if max_ma != element:
-        temp_ma = your_list[max_ma]
-        your_list[max_ma] = your_list[element]
-        your_list[element] = temp_ma
-        heapify(your_list, max_ma)
-    return your_list
-
+import heapq
 
 n = int(input())
 command = []
@@ -55,7 +13,8 @@ for j in range(n):
         if j < 3:
             print("No reviews yet")
         else:
-            my_list = heap_sort(my_list)
-            print(my_list)
+            res = heapq.nlargest(int(my_list.__len__() / 3),  my_list)
+            print(res[res.__len__() - 1])
+
     else:
-        my_list.append(int(command[j].split(" ")[1]))
+        heapq.heappush(my_list, int(command[j].split(" ")[1]))
