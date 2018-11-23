f6979e9a2ee9c80140a3bbe8a6fa35de1717f5c4
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-23 12:47:15 +0330

    start to get the graph from clients

---
+++ HW/2_Quera/Q4/derakht.py
@@ -0,0 +1,111 @@
+from collections import deque
+
+class Graph:
+    def __init__(self, v):
+        self.v = v
+        self.neighbors = deque()
+
+
+    def add_neighbor(self, neighbor):
+        self.neighbors.appendleft(neighbor)
+
+
+# public:
+
+# 	void addEdge(int v, int w);// function to add an edge to graph
+# 	void longestPathLength(); // prints longest path of the tree
+# 	pair<int, int> bfs(int u); // function returns maximum distant
+# 							// node from u with its distance
+# };
+#
+
+# void Graph::addEdge(int v, int w)
+# {
+# 	adj[v].push_back(w); // Add w to v’s list.
+# 	adj[w].push_back(v); // Since the graph is undirected
+# }
+
+
+# method returns farthest node and its distance from node u
+# pair<int, int> Graph::bfs(int u)
+# {
+# 	// mark all distance with -1
+# 	int dis[V];
+# 	memset(dis, -1, sizeof(dis));
+#
+# 	queue<int> q;
+# 	q.push(u);
+#
+# 	// distance of u from u will be 0
+# 	dis[u] = 0;
+#
+# 	while (!q.empty())
+# 	{
+# 		int t = q.front();	 q.pop();
+#
+# 		// loop for all adjacent nodes of node-t
+# 		for (auto it = adj[t].begin(); it != adj[t].end(); it++)
+# 		{
+# 			int v = *it;
+#
+# 			// push node into queue only if
+# 			// it is not visited already
+# 			if (dis[v] == -1)
+# 			{
+# 				q.push(v);
+#
+# 				// make distance of v, one more
+# 				// than distance of t
+# 				dis[v] = dis[t] + 1;
+# 			}
+# 		}
+# 	}
+#
+# 	int maxDis = 0;
+# 	int nodeIdx;
+#
+# 	// get farthest node distance and its index
+# 	for (int i = 0; i < V; i++)
+# 	{
+# 		if (dis[i] > maxDis)
+# 		{
+# 			maxDis = dis[i];
+# 			nodeIdx = i;
+# 		}
+# 	}
+# 	return make_pair(nodeIdx, maxDis);
+# }
+#
+# // method prints longest path of given tree
+# void Graph::longestPathLength()
+# {
+# 	pair<int, int> t1, t2;
+#
+# 	// first bfs to find one end point of
+# 	// longest path
+# 	t1 = bfs(0);
+#
+# 	// second bfs to find actual longest path
+# 	t2 = bfs(t1.first);
+#
+# 	cout << "Longest path is from " << t1.first << " to "
+# 		<< t2.first << " of length " << t2.second;
+# }
+#
+# // Driver code to test above methods
+
+n = int(input())
+
+nodes = [[0 for x in range(2)] for y in range(n)]
+
+for i in range(n):
+    nodes[i][0], nodes[i][1] = map(int, input().split())
+
+# graphs = []
+#
+# Graph g(n);
+# g.addEdge(0, 1);
+# g.addEdge(1, 2);
+# g.addEdge(2, 3);
+#
+# g.longestPathLength();
