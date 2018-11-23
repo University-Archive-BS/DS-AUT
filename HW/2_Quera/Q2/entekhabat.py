9fba065d079322e4ea3405b280fda92d1da30884
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-23 16:11:22 +0330

    yes yes it works completely

--- HW/2_Quera/Q2/entekhabat.py
+++ HW/2_Quera/Q2/entekhabat.py
@@ -2,18 +2,18 @@ import heapq
 
 n = int(input())
 groups = input().split()
-# groups.sort(key=int)
 
 my_list = []
 res = 0
 for i in range(n):
     heapq.heappush(my_list, int(groups[i]))
 
-print(my_list)
-for j in range(n):
-    if j == 0:
-        res += heapq.heappop(my_list) * n
-    else:
-        res += heapq.heappop(my_list) * (n - j + 1)
+temp = 0
+while my_list.__len__() > 1:
+    temp = heapq.heappop(my_list)
+    temp += heapq.heappop(my_list)
+    res += temp
+    heapq.heappush(my_list, temp)
 
+res += temp
 print(res)
