1c69e03c2836ac4b63c71a52bdedebb36f38e5a1
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-24 00:22:15 +0330

    statistics bst implemented but it doesn't work

--- HW/2_Quera/Q1/mashahir.py
+++ HW/2_Quera/Q1/mashahir.py
@@ -1,148 +1,50 @@
-# #include <stdio.h>
-# #include <stdlib.h>
-#
-# #define ARRAY_SIZE(arr) sizeof(arr)/sizeof(arr[0])
-#
-# typedef struct node_t node_t;
-#
-# /* Binary tree node */
-# struct node_t
-# {
-# 	int data;
-# 	int lCount;
-#
-# 	node_t* left;
-# 	node_t* right;
-# };
-#
-# /* Iterative insertion
-# Recursion is least preferred unless we gain something
-# */
-# node_t *insert_node(node_t *root, node_t* node)
-# {
-# 	/* A crawling pointer */
-# 	node_t *pTraverse = root;
-# 	node_t *currentParent = root;
-#
-# 	// Traverse till appropriate node
-# 	while(pTraverse)
-# 	{
-# 		currentParent = pTraverse;
-#
-# 		if( node->data < pTraverse->data )
-# 		{
-# 			/* We are branching to left subtree
-# 			increment node count */
-# 			pTraverse->lCount++;
-# 			/* left subtree */
-# 			pTraverse = pTraverse->left;
-# 		}
-# 		else
-# 		{
-# 			/* right subtree */
-# 			pTraverse = pTraverse->right;
-# 		}
-# 	}
-#
-# 	/* If the tree is empty, make it as root node */
-# 	if( !root )
-# 	{
-# 		root = node;
-# 	}
-# 	else if( node->data < currentParent->data )
-# 	{
-# 		/* Insert on left side */
-# 		currentParent->left = node;
-# 	}
-# 	else
-# 	{
-# 		/* Insert on right side */
-# 		currentParent->right = node;
-# 	}
-#
-# 	return root;
-# }
-#
-# /* Elements are in an array. The function builds binary tree */
-# node_t* binary_search_tree(node_t *root, int keys[], int const size)
-# {
-# 	int iterator;
-# 	node_t *new_node = NULL;
-#
-# 	for(iterator = 0; iterator < size; iterator++)
-# 	{
-# 		new_node = (node_t *)malloc( sizeof(node_t) );
-#
-# 		/* initialize */
-# 		new_node->data = keys[iterator];
-# 		new_node->lCount = 0;
-# 		new_node->left = NULL;
-# 		new_node->right = NULL;
-#
-# 		/* insert into BST */
-# 		root = insert_node(root, new_node);
-# 	}
-#
-# 	return root;
-# }
-#
-# int k_smallest_element(node_t *root, int k)
-# {
-# 	int ret = -1;
-#
-# 	if( root )
-# 	{
-# 		/* A crawling pointer */
-# 		node_t *pTraverse = root;
-#
-# 		/* Go to k-th smallest */
-# 		while(pTraverse)
-# 		{
-# 			if( (pTraverse->lCount + 1) == k )
-# 			{
-# 				ret = pTraverse->data;
-# 				break;
-# 			}
-# 			else if( k > pTraverse->lCount )
-# 			{
-# 				/* There are less nodes on left subtree
-# 					Go to right subtree */
-# 				k = k - (pTraverse->lCount + 1);
-# 				pTraverse = pTraverse->right;
-# 			}
-# 			else
-# 			{
-# 				/* The node is on left subtree */
-# 				pTraverse = pTraverse->left;
-# 			}
-# 		}
-# 	}
-#
-# 	return ret;
-# }
-#
-# /* Driver program to test above functions */
-# int main(void)
-# {
-# 	/* just add elements to test */
-# 	/* NOTE: A sorted array results in skewed tree */
-# 	int ele[] = { 20, 8, 22, 4, 12, 10, 14 };
-# 	int i;
-# 	node_t* root = NULL;
-#
-# 	/* Creating the tree given in the above diagram */
-# 	root = binary_search_tree(root, ele, ARRAY_SIZE(ele));
-#
-# 	/* It should print the sorted array */
-# 	for(i = 1; i <= ARRAY_SIZE(ele); i++)
-# 	{
-# 		printf("\n kth smallest elment for k = %d is %d",
-# 				i, k_smallest_element(root, i));
-# 	}
-#
-# 	getchar();
-# 	return 0;
-# }
+class Node:
+    def __init__(self):
+        self.value = 0
+        self.count = 0
+        self.right = None
+        self.left = None
+
+    def set_value(self, value):
+        self.value = value
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def set_root(self, root):
+        self.root = root
+
+    def insert_node(self, new_node):
+        traversal_node = self.root
+        current_node = self.root
+        while traversal_node:
+            current_node = traversal_node
+            if new_node.value < traversal_node.value:
+                traversal_node.count += 1
+                traversal_node = traversal_node.left
+            else:
+                traversal_node = traversal_node.right
+        if new_node.value < current_node.value:
+            current_node.left = new_node
+        else:
+            current_node.right = new_node
+
+
+    def Kth_smallest(self, k):
+        ret = -1
+        traverse_node = self.root
+        while traverse_node:
+            if traverse_node.count == (k - 1):
+                ret = traverse_node.value
+            elif traverse_node.value < k:
+                k = k - (traverse_node.count + 1)
+                traverse_node = traverse_node.right
+            else:
+                traverse_node = traverse_node.left
+        return ret
+
 
 n = int(input())
 command = []
@@ -150,15 +52,26 @@ command = []
 for i in range(n):
     command.append(input())
 
-my_list = []
+numbers = 0
+
+bst = BinarySearchTree()
 
 for j in range(n):
     if command[j].__len__() == 1:
         if j < 3:
             print("No reviews yet")
         else:
-            res = heapq.nlargest(int(my_list.__len__() / 3),  my_list)
-            print(res[res.__len__() - 1])
+            print(bst.root.left.value)
+            # for t in range(numbers):
+            #     print(bst.Kth_smallest(t))
+            # print(bst.Kth_smallest(numbers - int(numbers / 3)))
 
     else:
-        heapq.heappush(my_list, int(command[j].split(" ")[1]))
+        temp_man = Node()
+        temp_man.set_value(int(command[j].split(" ")[1]))
+        if numbers == 0:
+            bst.set_root(temp_man)
+        else:
+            bst.insert_node(temp_man)
+        numbers += 1
+
