5d3e586c48e0c05d5d4fa375abf7af0d94ef3ca6
Ali Nazari <ali.nazari@aut.ac.ir>
2018-11-24 22:08:21 +0330

    implement Q4 semi complete

--- HW/2_Quera/Q4/derakht.py
+++ HW/2_Quera/Q4/derakht.py
@@ -1,28 +1,42 @@
-from collections import deque
+from collections import defaultdict
+from itertools import chain
 
 
-class Graph:
-    def __init__(self, v):
-        self.v = v
-        self.neighbors = [deque()] * v
+def DFS(G, v, seen=None, path=None):
+    if seen is None: seen = []
+    if path is None: path = [v]
 
-    def add_neighbor(self, neighbor):
-        self.neighbors.appendleft(neighbor)
+    seen.append(v)
+
+    paths = []
+    for t in G[v]:
+        if t not in seen:
+            t_path = path + [t]
+            paths.append(tuple(t_path))
+            paths.extend(DFS(G, t, seen[:], t_path))
+    return paths
 
 
 n = int(input())
 
-nodes = [[0 for x in range(2)] for y in range(n)]
+nodes = [[0 for x in range(2)] for y in range(n - 1)]
 
 numbers = []
-for i in range(n):
-    nodes[i][0], nodes[i][1] = map(int, input().split())
+for i in range(n - 1):
+    nodes[i][0], nodes[i][1] = map(str, input().split())
     if not nodes[i][0] in numbers:
         numbers.append(nodes[i][0])
 
     if not nodes[i][1] in numbers:
         numbers.append(nodes[i][1])
 
-graph = Graph(5)
-graphs = []
+G = defaultdict(list)
+for (s, t) in nodes:
+    G[s].append(t)
+    G[t].append(s)
 
+all_paths = list(chain.from_iterable(DFS(G, n) for n in set(G)))
+max_len = max(len(p) for p in all_paths)
+max_paths = [p for p in all_paths if len(p) == max_len]
+
+print(max_len - 1)
